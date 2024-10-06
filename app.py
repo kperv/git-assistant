6721890e42aea7fdabfec316514a12917b285fd6
Ksenia Pervova <kvpervova@gmail.com>
2024-10-06 18:46:40 +0200

    Work on app

--- app.py
+++ app.py
@@ -1,9 +1,19 @@
 import streamlit as st
-import pandas as pd
-import numpy as np
 
-map_data = pd.DataFrame(
-    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
-    columns=['lat', 'lon'])
+# with st.sidebar:
 
-st.map(map_data)
+st.title("💬 Git Assistant")
+st.caption("🚀 Pro-Git enhanced with AI")
+if "messages" not in st.session_state:
+    st.session_state["messages"] = [{"role": "assistant", "content": "How to commit changes in a file?"}]
+
+for msg in st.session_state.messages:
+    st.chat_message(msg["role"]).write(msg["content"])
+
+if prompt := st.chat_input():
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    st.chat_message("user").write(prompt)
+    response = client.chat.completions.create(model="gpt-3.5-turbo", messages=st.session_state.messages)
+    msg = response.choices[0].message.content
+    st.session_state.messages.append({"role": "assistant", "content": msg})
+    st.chat_message("assistant").write(msg)

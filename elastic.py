638b6c79039398c438ac4c6514817a229b1870b2
Ksenia Pervova <kvpervova@gmail.com>
2024-10-06 17:56:49 +0200

    Add rag

--- elastic.py
+++ elastic.py
@@ -6,12 +6,14 @@ class DocumentsRetriver:
     elastic_url: str = 'http://localhost:9200'
     index_name: str = "pro-git-book"
 
-    def __init__(self):
+    def __init__(self, num_docs: int = 2):
+        self.num_docs = num_docs
         self.es_client = Elasticsearch(self.elastic_url)
         book = pd.read_csv('book.csv')
         self.documents = book.to_dict(orient='records')
+        self.index_documents()
 
-    def index_documenets(self):
+    def index_documents(self):
         index_settings = {
             "settings": {
                 "number_of_shards": 1,
@@ -30,23 +32,30 @@ class DocumentsRetriver:
             self.es_client.index(index=self.index_name, document=doc)
 
     def find_documents(self, query: str):
-        search_query = {
-            "size": 2,
-            "query": {
-                "bool": {
-                    "must": {
-                        "multi_match": {
-                            "query": query,
-                            "fields": ["text", "chapter"],
-                            "type": "best_fields"
-                        }
-                    },
+        relevant_documents = []
+        if not self.is_valid_query(query):
+            return relevant_documents
+        else:
+            search_query = {
+                "size": self.num_docs,
+                "query": {
+                    "bool": {
+                        "must": {
+                            "multi_match": {
+                                "query": query,
+                                "fields": ["text", "chapter"],
+                                "type": "best_fields"
+                            }
+                        },
+                    }
                 }
             }
-        }
-        response = self.es_client.search(index=self.index_name, body=search_query)
-        relevant_texts = [item['_source']['text'] for item in response['hits']['hits']]
-        return relevant_texts
+            response = self.es_client.search(index=self.index_name, body=search_query)
+            relevant_documents = [item['_source']['text'] for item in response['hits']['hits']]
+            return relevant_documents
+
+    def is_valid_query(self, query: str):
+        return True if (query and query.isalpha()) else False
 
 
 def main():
---
+++ rag.py
@@ -0,0 +1,40 @@
+from openai import OpenAI
+
+from elastic import DocumentsRetriver
+
+
+class GitAssistant:
+    def __init__(self):
+        self.client = OpenAI(
+            base_url='http://localhost:11434/v1/',
+            api_key='ollama',
+        )
+        self.retriever = DocumentsRetriver()
+
+    def search_book(self, question: str):
+        return self.retriever.find_documents(question)
+
+    def build_prompt(self, question: str, context: list[str]):
+        context_docs = " \n".join(context)
+        prompt = f"""
+            Analyze manual documents and answer the user question: {question}. 
+            Generate a concise list of steps to implement.
+            
+            Manual:
+            {context_docs}
+        """
+        return prompt
+
+    def response(self, prompt: str):
+        response = self.client.chat.completions.create(
+            model='llama3',
+            messages=[{"role": "user", "content": prompt}],
+            temperature=0.5
+        )
+        return response.choices[0].message.content
+
+    def answer(self, question: str):
+        context = self.search_book(question)
+        prompt = self.build_prompt(question, context)
+        answer = self.response(prompt)
+        return answer
